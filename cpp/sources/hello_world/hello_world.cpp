04abdb7b3af3b0a6fe91acdc36e8b76c40cc1d97
bvb231 <torsten.molina@gmail.com>
2022-12-27 00:00:14 -0500

    Have some sort of make files going...

---
+++ cpp/sources/hello_world/hello_world.cpp
@@ -0,0 +1,9 @@
+#include "hello_world.h"
+// Definition of user-defined function doPrint()
+void hello_world() // doPrint() is the called function in this example
+{
+    std::cout << "hello_world()\n";
+	return;
+}
+
+
