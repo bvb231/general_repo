89c3126d7f4703abba738d443dc10e3d0e14fa6f
bvb231 <torsten.molina@gmail.com>
2024-01-28 11:23:37 -0500

    Updated a yaml file

--- sv/cores/network/network_decoder/network_decoder.sv
+++ sv/cores/network/network_decoder/network_decoder.sv
@@ -7,16 +7,15 @@
  *
  *
  */
-module network_decoder
-(
-    input logic CLK, 
+module network_decoder (
+    input logic CLK,
     input logic RST,
 
     //Input bus from MAC
     input logic i_valid,
     input logic [127:0] i_data,
-    input logic i_sop, 
-    input logic i_eop, 
+    input logic i_sop,
+    input logic i_eop,
     input logic i_ebp,
     input logic [5:0] i_byte_cnt,
 
@@ -26,36 +25,36 @@ module network_decoder
 );
 
 
-logic [127:0] data_a;
-logic [7:0] word_count_a;
-logic valid_a;
+  logic [127:0] data_a;
+  logic [7:0] word_count_a;
+  logic valid_a;
 
-always_ff@(posedge CLK) begin 
-    if(RST) begin 
-        word_count_a <= 8'd0;
-    end else begin 
-        if(i_valid) begin 
-            if(i_eop || i_sop) begin 
-                word_count_a <= 8'd0;
-            end else begin 
-                word_count_a <= word_count_a + 8'd1;
-            end
-        end 
+  always_ff @(posedge CLK) begin
+    if (RST) begin
+      word_count_a <= 8'd0;
+    end else begin
+      if (i_valid) begin
+        if (i_eop || i_sop) begin
+          word_count_a <= 8'd0;
+        end else begin
+          word_count_a <= word_count_a + 8'd1;
+        end
+      end
     end
-end
+  end
 
 
-always_ff@(posedge CLK) begin 
-    data_a <= i_data;
+  always_ff @(posedge CLK) begin
+    data_a  <= i_data;
     valid_a <= i_valid;
-end
+  end
 
-//Note - Currently we do not support any VLANs
-always_ff@(posedge CLK) begin 
+  //Note - Currently we do not support any VLANs
+  always_ff @(posedge CLK) begin
     o_mac_destination <= data_a[127-:48];
     o_mac_source <= data_a[(127-48)-:48];
     o_ether_type <= data_a[(127-96)-:16];
-end
+  end
 
 
 
