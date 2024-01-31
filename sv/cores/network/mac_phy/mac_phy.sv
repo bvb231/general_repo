fb82aca1f4763402d75ecd00da616f88b0fe4445
bvb231 <torsten.molina@gmail.com>
2024-01-31 10:00:53 -0500

    updates to the crc calculator

--- sv/cores/network/mac_phy/mac_phy.sv
+++ sv/cores/network/mac_phy/mac_phy.sv
@@ -1,5 +1,41 @@
 module mac_phy (
-    input logic CLK
+    input logic CLK,
+
+    /*
+        TX AND RX are both are @ 125 MHz
+    */
+    output logic [7:0] TX_D,
+    output logic TX_EN,
+    output logic TX_ERR,
+    output logic TX_CLK,
+
+    input logic [7:0] RX_D,
+    input logic RX_EN,
+    input logic RX_ERR,
+    input logic RX_CLK
 );
+
+
+  /*TX FIFO Interface*/
+
+  /*RX FIFO Interface*/
+
+
+
+
+  // TX Datapath
+
+
+
+
+
+
+
+
+  //RX Datapath
+
+
+
+
 
 endmodule
