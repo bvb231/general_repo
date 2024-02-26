9cfa8f9a656e10dd3434ae1a62489dfdf3cb73fa
Torsten Molina <torsten.molina@gmail.com>
2024-02-25 21:02:17 -0500

    more stuff

--- octave/transmission_model.m
+++ octave/transmission_model.m
@@ -3,21 +3,42 @@ clear all;
 clc;
 
 %Transmission end
+modulation = 16;
+symbols = 100;
 
 symbol_padding = 4;
 
-random_numbers = randi([0,1],[1,100]);
+random_numbers = randi([0,1],[1,modulation*symbols]);
 %random_numbers = ones(1,100);
 
-rand_num_zero_padded = zeros(1,symbol_padding*length(random_numbers));
 
-rand_num_zero_padded([1:symbol_padding:end])=random_numbers;
+%Select modulation scheme
+
+%QAM-4
+
+%We sliceoutu our bit squence into chunks of 16 to feed into our modulator
+bit_chunks = reshape(random_numbers,sqrt(modulation),[]);
+
+for a = 1:size(bit_chunks,2)
+
+  dummy(a) = bin2dec(char(bit_chunks(:,a)'+'0'));
+  modulated_signal(a) = qammod(dummy(a),modulation);
+endfor
+
+
+
+modulated_zero_padded = zeros(1,symbol_padding*length(modulated_signal));
+
+modulated_zero_padded([1:symbol_padding:end])=modulated_signal;
 
 
 root_raised_cosine = rcosfir(0.25,4,4,1,'sqrt');
 
 
-output = conv(root_raised_cosine,rand_num_zero_padded);
+
+output_real = conv(root_raised_cosine,real(modulated_zero_padded));
+
+output_imag= conv(root_raised_cosine,imag(modulated_zero_padded));
 
 
 %plot(output)
